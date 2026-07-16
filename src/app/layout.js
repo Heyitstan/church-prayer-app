5bfa3b22dcee15bc4ecd4c63f1c68bf73999fff8
Tan Huynh <txhuynh@uci.edu>
2026-07-16 13:12:25 -0500

    Added white background to logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Church Prayer Wall",
   description: "Share and support community prayer requests.",
-  manifest:'/manifest.json',
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
